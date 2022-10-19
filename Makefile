2728df1513239ab02b6f67f88448d3c9d34098a4
Andy <andy_chang@protech.com.tw>
2022-10-19 18:04:34 +0800

    study makefile

--- Makefile
+++ Makefile
@@ -1,21 +1,28 @@
+POSTGRES_DB_SCHEMA := v1
+
+
+
 postgres:
 	docker run --name postgres12 -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=admin -d postgres:12-alpine
 
 createdb:
-	docker exec -it postgres12 createdb --username=root --owner=root simple_bank
+	docker exec -it postgres12 createdb --username=root --owner=root simple_bank 
+
+createSchema:
+	docker exec -it postgres12 psql -U root \connect simple123;CREATE SCHEMA v3;exit
 
 dropdb:
 	docker exec -it postgres12 dropdb simple_bank
 
 migrateup:
-	migrate -path db/migration -database "postgresql://root:admin@localhost:5432/simple_bank?sslmode=disable" -verbose up
+	migrate -path db/migration -database "postgresql://root:admin@localhost:5432/simple_bank?sslmode=disable&search_path=${POSTGRES_DB_SCHEMA}" -verbose up
 
 migratedown:
-	migrate -path db/migration -database "postgresql://root:admin@localhost:5432/simple_bank?sslmode=disable" -verbose down
+	migrate -path db/migration -database "postgresql://root:admin@localhost:5432/simple_bank?sslmode=disable&search_path=${POSTGRES_DB_SCHEMA}" -verbose down
 
 sqlc:
 	docker run --rm -v D:\bank\simplebank:/src -w /src kjconroy/sqlc generate
 test:
 	go test -v -cover ./...
 
-.PHONY:postgres createdb dropdb migrateup migratedown sqlc test
+.PHONY:postgres createdb dropdb migrateup migratedown sqlc test createSchema
--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,6 +12,7 @@ func TestTransferTx(t *testing.T) {
 	store := NewStore(testDB)
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
 
 	errs := make(chan error)
 	results := make(chan TransferTxResult)
@@ -30,6 +32,7 @@ func TestTransferTx(t *testing.T) {
 	}
 
 	// 檢查結果
+	existed := make(map[int]bool)
 	for i := 0; i < n; i++ {
 		err := <-errs
 		require.NoError(t, err)
@@ -47,7 +50,7 @@ func TestTransferTx(t *testing.T) {
 		_, err = store.GetTransfer(context.Background(), transfer.ID)
 		require.NoError(t, err)
 
-		// 檢查帳戶紀錄
+		// 檢查紀錄
 		fromEntry := result.FromEntry
 		require.NotEmpty(t, fromEntry)
 		require.Equal(t, account1.ID, fromEntry.AccountID)
@@ -68,5 +71,37 @@ func TestTransferTx(t *testing.T) {
 		_, err = store.GetEntry(context.Background(), toEntry.ID)
 		require.NoError(t, err)
 
+		// check accounts
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		// check balances
+		fmt.Println(">> tx:", fromAccount.Balance, toAccount.Balance)
+		diff1 := account1.Balance - fromAccount.Balance
+		diff2 := toAccount.Balance - account2.Balance
+		require.Equal(t, diff1, diff2)
+		require.True(t, diff1 > 0)
+		require.True(t, diff1%amount == 0)
+
+		k := int(diff1 / amount)
+		require.True(t, k >= 1 && k <= n)
+		require.NotContains(t, existed, k)
+		existed[k] = true
 	}
+
+	updateAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updateAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after:", account1.Balance, account2.Balance)
+	require.Equal(t, account1.Balance-int64(n)*amount, updateAccount1.Balance)
+	require.Equal(t, account2.Balance-int64(n)*amount, updateAccount2.Balance)
+
 }
